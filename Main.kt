185b211916482167c0f23fda80e1c055686167f8
Aaronmtz1149 <111892655+Aaronmtz1149@users.noreply.github.com>
2022-09-19 08:21:17 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,21 @@
+/*fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}*/
+fun main()
+{
+    /* You don't need to declare the variable all the time (line 13-14)*/
+    var friendsCount = 300
+    println(friendsCount)
+    if(friendsCount>500)
+    {
+        println("Adopt a dog today!!")
+    }else
+    {
+        println("Buy cat food at 20%")
+    }
+
+}
